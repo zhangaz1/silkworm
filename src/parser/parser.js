27760f6f7c6e5b3f5c6b366fe3f98b92b6e45607
zhangaz1 <zhangaz1@hotmail.com>
2017-07-31 10:02:15 +0800

    refac

--- src/parser/parser.js
+++ src/parser/parser.js
@@ -1,13 +1,38 @@
 'use strict';
 
 
-module.exports = parser;
+class Parser {
 
-function parser(express){
-    express = express.trim();
-
-    if(express === ''){
-        return {};
+    constructor(fields){
+        this.fields = fields;
     }
 
+    parse(originalExpress){
+        let express = originalExpress.trim();
+
+        if(express === ''){
+            return {};
+        }
+    }
 }
+
+module.exports = Parser;
+
+/*
+*
+* 替换带引号的value
+*
+* in 等特殊操作符的值的处理
+*
+* 按括号拆分（空括号处理：函数调用）
+* 解析子表达式
+*
+* 按逻辑运算符从低王高拆分
+* 然后分别对字表达式重复此过程
+*
+* 识别单个表达式
+* 识别字段
+* 识别字段操作符
+* 识别value
+*
+ */
--- test/parser/parser.test.js
+++ test/parser/parser.test.js
@@ -3,13 +3,15 @@
 
 let should = require('should');
 
-let parser = require('./../../src/parser/parser.js');
+let Parser = require('./../../src/parser/parser.js');
 
 describe('test parser', function(){
 
     it('should can parser empty string', function(){
         let express = ' ';
-        let result = parser(express);
+        let parser = new Parser([]);
+
+        let result = parser.parse(express);
 
         result.should.be.an.Object()
             .and.eql({});
