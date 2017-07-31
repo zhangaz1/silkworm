d2739971b410b03ee0b1e969bdc1b75394b78a4b
zhangaz1 <zhangaz1@hotmail.com>
2017-07-31 10:01:44 +0800

    add models

---
+++ src/model/field/field.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+module.exports = Field;
+
+class Field {
+    constructor(name,
+                operators){
+        this.name = name;
+        this.operators = operators;
+    }
+}
